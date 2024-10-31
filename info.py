d2ffcced85e07a56b4647df924ba2a96d4172535
darkmovies <darkmovies470@gmail.com>
2024-10-31 15:44:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID', "20247467"])
-API_HASH = environ['API_HASH', "8ab4a0d75eec6fe40b85144c2c0ff418"]
+API_ID = int(environ['API_ID', '20247467'])
+API_HASH = environ['API_HASH', '8ab4a0d75eec6fe40b85144c2c0ff418']
 BOT_TOKEN = environ['BOT_TOKEN', "7183447790:AAFawsUBChuDVyFA5Z920hZJD7wud-yP9JI"]
 
 # Bot settings
