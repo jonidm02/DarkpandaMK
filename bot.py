db3d4e88fe6dd23567c8874137865a41b9cebcf1
Joni <152844226+jonidm02@users.noreply.github.com>
2025-01-30 23:43:31 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
+from database.ia_filterdb import Media2
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT
 from utils import temp
